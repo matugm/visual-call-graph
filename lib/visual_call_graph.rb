af395e0547b9621f3a7e9dde7df7f591891a759b
Jesus Castello <matugm@gmail.com>
2016-12-29 00:03:46 +0100

    Initial commit

---
+++ lib/visual_call_graph.rb
@@ -0,0 +1,47 @@
+
+require 'graphviz'
+
+module VisualCallGraph
+  extend self
+
+  def trace
+    stack  = ["start"]
+    edges  = []
+
+    g = GraphViz.new(:G, :type => :digraph)
+
+    g.add_node("start")
+
+    trace =
+    TracePoint.new(:call, :return) do |event|
+      case event.event
+      when :return then stack.pop
+      when :call   then add_edges(event, edges, stack, g)
+      end
+    end
+
+    trace.enable
+    yield
+    trace.disable
+
+    g.output(png: "call_graph.png")
+
+    puts "Call graph created with a total of #{g.node_count} #{g.node_count > 1 ? 'nodes' : 'node'}."
+  end
+
+  private
+
+  def add_edges(event, edges, stack, g)
+    node = "#{event.defined_class}##{event.method_id}".freeze
+
+    g.add_node(node)
+    stack << node
+
+    edge = [stack[-2], stack[-1]]
+
+    return if edges.include?(edge)
+
+    edges << edge
+    g.add_edge(*edges.last)
+  end
+end
---
+++ visual_call_graph.gemspec
@@ -0,0 +1,13 @@
+
+Gem::Specification.new do |s|
+  s.name        = 'visual_call_graph'
+  s.version     = '0.1.0'
+  s.date        = '2016-12-28'
+  s.summary     = ""
+  s.description = "Uses the TracePoint class & Graphviz to generate a visual representation of all the methods called by another method."
+  s.authors     = ["Jesus Castello"]
+  s.email       = 'jesus@blackbytes.info'
+  s.files       = ["lib/visual_call_graph.rb"]
+  s.license     = 'MIT'
+end
+
