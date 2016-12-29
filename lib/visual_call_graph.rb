d2b2481946a92441ebeff3679c281825fc1e54ea
Jesus Castello <matugm@gmail.com>
2016-12-29 02:35:12 +0100

    Convert to instance variables

--- lib/visual_call_graph.rb
+++ lib/visual_call_graph.rb
@@ -5,18 +5,18 @@ module VisualCallGraph
   extend self
 
   def trace
-    stack  = ["start"]
-    edges  = []
+    @stack  = ["start"]
+    @edges  = []
 
-    g = GraphViz.new(:G, :type => :digraph)
+    @g = GraphViz.new(:G, :type => :digraph)
 
-    g.add_node("start")
+    @g.add_node("start")
 
     trace =
     TracePoint.new(:call, :return) do |event|
       case event.event
-      when :return then stack.pop
-      when :call   then add_edges(event, edges, stack, g)
+      when :return then @stack.pop
+      when :call   then add_edges(event)
       end
     end
 
@@ -24,24 +24,24 @@ module VisualCallGraph
     yield
     trace.disable
 
-    g.output(png: "call_graph.png")
+    @g.output(png: "call_graph.png")
 
-    puts "Call graph created with a total of #{g.node_count} #{g.node_count > 1 ? 'nodes' : 'node'}."
+    puts "Call graph created with a total of #{@g.node_count} #{@g.node_count > 1 ? 'nodes' : 'node'}."
   end
 
   private
 
-  def add_edges(event, edges, stack, g)
+  def add_edges(event)
     node = "#{event.defined_class}##{event.method_id}".freeze
 
-    g.add_node(node)
-    stack << node
+    @g.add_node(node)
+    @stack << node
 
-    edge = [stack[-2], stack[-1]]
+    edge = [@stack[-2], @stack[-1]]
 
-    return if edges.include?(edge)
+    return if @edges.include?(edge)
 
-    edges << edge
-    g.add_edge(*edges.last)
+    @edges << edge
+    @g.add_edge(*@edges.last)
   end
 end
